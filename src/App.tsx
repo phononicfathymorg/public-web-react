f8119d0d67bbc7e4c6eb08afaeb094991b636c62
Jeremy Tomlinson <jeremy.tomlinson@gmail.com>
2022-05-13 14:58:18 -0600

    Updates for Phononic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './powerfleet-logo-80.jpg';
+import logo from './phononic380.jpg';
 import frameworkImage from './react.jpg';
 import './App.css';
 
@@ -7,22 +7,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="https://powerfleet.fathym-it.com/"><img src={logo} className="pf-logo" alt="logo" /></a>
+        <a href="https://phononic.fathym-it.com/"><img src={logo} className="pf-logo" alt="logo" /></a>
         <div className="siteNav">
-          <a href="https://powerfleet.fathym-it.com/dash1/">
+          <a href="https://phononic.fathym-it.com/dash1/">
             <button className="navbutton">Dash 1</button>
           </a>
-          <a href="https://powerfleet.fathym-it.com/dash2/">
+          <a href="https://phononic.fathym-it.com/dash2/">
             <button className="navbutton">Dash 2</button>
           </a>
-          <a href="https://powerfleet.fathym-it.com/dash3/">
+          <a href="https://phononic.fathym-it.com/dash3/">
             <button className="navbutton">Dash 3</button>
           </a>
         </div>
       </header>
       <br />
       <div className="dashboard">
-        <iframe title="powerfleet-LV450-reefer-tracking - Page 1" className="responsive-iframe" src="https://app.powerbi.com/view?r=eyJrIjoiOGJjYTA0MzktMWY4MS00M2I4LTkzYTctMjRmNzQ1YjU1YjU5IiwidCI6IjZkY2JlYmQwLWY4ZDAtNGE5ZC04OWU1LTU4NzNlODE0NmIwYSIsImMiOjR9" frameBorder="0"></iframe>
+        <iframe title="phononic-thermal-tracking" className="responsive-iframe" src="https://app.powerbi.com/view?r=eyJrIjoiNTFlNjAwYWUtOWY4Yi00ZDE4LWE2MmQtNDI1MTkxOTdkZmIwIiwidCI6IjZkY2JlYmQwLWY4ZDAtNGE5ZC04OWU1LTU4NzNlODE0NmIwYSIsImMiOjR9" frameBorder="0"></iframe>
       </div>
       <br />
       <div className="builtin">
